687e2c5342690d215e95883e3c20daa619ab296b
StevenStrij <ststrijakov@gmail.com>
2015-01-06 17:02:28 +1100

    Adding in all files

--- lcdDis/raiseDongers/raiseDongers.ino
+++ lcdDis/raiseDongers/raiseDongers.ino
@@ -1,23 +1,100 @@
 #include <LiquidCrystal.h>
 
+#define BUTTON 14
+#define LATCH 11
+#define DATA 12
+#define CLOCK 10
+
 LiquidCrystal lcd(6, 7, 8, 2, 3, 4, 5);
 
-byte glyph[8] {
-   B10001,
-   B01010,
-   B01010,
+byte lSide[8] {
+   B00001,
+   B00010,
    B00100,
-   B00100,
-   B01010,
-   B01010,
-   B10001
+   B01000,
+   B01000,
+   B01000,
+   B10000,
+   B10000
 };
+
+byte arm[8] {
+   B00000,
+   B00000,
+   B00000,
+   B11110,
+   B00001,
+   B00010,
+   B11100,
+   B00000
+};
+
+byte eye[8] {
+   B00000,
+   B00100,
+   B01110,
+   B11111,
+   B01010,
+   B00100,
+   B00000,
+   B00000
+};
+
+byte rSide[8] {
+   B10000,
+   B01000,
+   B00100,
+   B00010,
+   B00010,
+   B00010,
+   B00001,
+   B00001
+};
+
+int buttonState;
 
 void setup() {
    // put your setup code here, to run once:
-   lcd.createChar(0, glyph);
+   pinMode(BUTTON, INPUT);
+   
+   pinMode(LATCH, OUTPUT);
+   pinMode(DATA, OUTPUT);
+   pinMode(CLOCK, OUTPUT);
+   
+   lcd.createChar(0, lSide);
+   lcd.createChar(1, arm);
+   lcd.createChar(2, eye);
+   lcd.createChar(3, rSide);
    lcd.begin(16,2);
-   lcd.write(byte(0));
+   
+   digitalWrite(BUTTON, HIGH);
+   
+
 }
 
-void loop() {}
+void ledWrite(byte a) {
+   digitalWrite(LATCH, LOW);
+   shiftOut(DATA, CLOCK, LSBFIRST, a);
+   digitalWrite(LATCH, HIGH);
+}
+
+void loop() {  
+  if (digitalRead(BUTTON) == LOW) {
+     printDonger();
+  } else {
+     lcd.clear();
+  }
+}
+
+void printDonger() {
+   lcd.setCursor(0,0);
+   lcd.write(byte(0));
+   lcd.write(byte(1));
+   lcd.write(byte(2));
+   lcd.write("_");
+   lcd.write(byte(2));
+   lcd.write(byte(3));
+   lcd.write(byte(1));
+   lcd.setCursor(0,1);
+   lcd.write("GIVE DIRETIDE!");
+}
---
+++ playMusic/playMusic.ino
@@ -0,0 +1,110 @@
+
+#include <LiquidCrystal.h>
+
+#define START 128
+#define SAFE 131
+
+#define STRAIGHT 32768
+#define DRIVE 137
+
+#define SCRIPT 152
+#define WAIT_DIST 156
+
+#define POTENT 3
+
+int vel = 0;
+
+LiquidCrystal lcd(6, 7, 8, 2, 3, 4, 5);
+
+void sendInstruction(int inst) {
+   Serial.write(inst);
+   delay(100);
+}
+
+void moveDist(int dist, int vel) {
+   float timeToDrive = (float)dist/(float)vel;
+   
+   sendInstruction(SCRIPT);
+   sendInstruction(13);
+   
+   sendInstruction(DRIVE);
+   sendInstruction(highByte(vel));
+   sendInstruction(lowByte(vel));
+   sendInstruction(highByte(STRAIGHT));
+   sendInstruction(lowByte(STRAIGHT));
+   
+   sendInstruction(WAIT_DIST);
+   sendInstruction(highByte(dist));
+   sendInstruction(lowByte(dist));
+   
+   sendInstruction(DRIVE);
+   sendInstruction(highByte(0));
+   sendInstruction(lowByte(0));
+   sendInstruction(highByte(STRAIGHT));
+   sendInstruction(lowByte(STRAIGHT));
+}
+
+void move(int vel, float degOfRot) {
+
+   sendInstruction(DRIVE);
+   sendInstruction(highByte(vel));
+   sendInstruction(lowByte(vel));
+   sendInstruction(highByte((int)(degOfRot*2000)));
+   sendInstruction(lowByte((int)(degOfRot*2000)));
+   
+
+  
+}
+
+void setup() {
+   Serial.begin(57600);
+   lcd.begin(16, 2);
+
+   sendInstruction(START);
+   sendInstruction(SAFE);
+   
+   pinMode(14, INPUT);
+   pinMode(15, INPUT);
+   
+   digitalWrite(14, HIGH);
+   digitalWrite(15, HIGH);
+}
+
+void loop() {
+   int deg = analogRead(POTENT);
+   
+   float perRot = 0.00;
+   deg -= 512;
+   
+   perRot = (float)deg/512;
+   if (perRot < 0) {
+     perRot = -1.00f - perRot;
+   } else {
+     perRot = 1.00f - perRot; 
+   }
+   
+   if (deg == 0) {
+      perRot = -0.01;
+   }
+   
+   if (digitalRead(14) == LOW) {
+      vel += 50;
+   } else if (digitalRead(15) == LOW) {
+      vel -= 50;
+   }
+   
+   if (vel < -500) {
+      vel = -500;
+   } else if (vel > 500) {
+      vel = 500; 
+   }
+   
+   String str = String(vel);
+   char test[4];
+   str.toCharArray(test, 4);
+   lcd.clear();
+   lcd.print(test);
+   
+   move(vel, perRot);
+   delay(200);
+}
